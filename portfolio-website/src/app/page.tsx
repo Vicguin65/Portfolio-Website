57d238a6861952f8ff711f809afd328e97949ef2
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-02-25 16:23:14 -0500

    Initial Commit

---
+++ portfolio-website/src/app/page.tsx
@@ -0,0 +1,14 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Tyler's Portfolio Website</h1>
+      <MyButton></MyButton>
+    </div>
+  );
+}
