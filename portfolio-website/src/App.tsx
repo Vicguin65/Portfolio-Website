1d1e5c48c4dc34976437619edeb79032c5cc263f
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-03-07 16:01:35 -0500

    start of landing page

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import tyler from "./assets/tyler-removebg-preview.png";
+import thought from "./assets/thoughtbubble.png";
+import land from "./assets/landbetter.jpg";
+import moon from "./assets/moon.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Parallax pages={5}>
+      <style>background-image: linear-gradient(red, yellow)</style>
+      <ParallaxLayer
+        offset={0}
+        speed={1}
+        factor={2.1}
+        style={{
+          backgroundImage: `url(${moon})`,
+          backgroundSize: "cover",
+        }}
+      ></ParallaxLayer>
+      <ParallaxLayer
+        offset={3}
+        speed={1}
+        factor={3}
+        style={{
+          backgroundImage: `url(${land})`,
+          backgroundSize: "cover",
+        }}
+      ></ParallaxLayer>
+      <ParallaxLayer
+        className="linear-gradient"
+        offset={1}
+        factor={4.1}
+        speed={1}
+      ></ParallaxLayer>
+      <ParallaxLayer sticky={{ start: 0.3, end: 3.4 }} className="cloud">
+        <img src={thought} className="img-fluid" alt="..." />
+      </ParallaxLayer>
+    </Parallax>
+  );
 }
 
-export default App
+export default App;
