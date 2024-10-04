d799c9750a258ed24e722a2a27f95d5b26882a8d
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-10-04 17:27:09 -0400

    initial start of pages

--- portfolio-website/src/pages/HomePage.tsx
+++ portfolio-website/src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 // src/pages/HomePage.tsx
-import React from 'react';
+import React from "react";
 
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <section>
+        <h5>Welcome to my portfolio webpage!</h5>
+        <text>
+          This webpage lists everything about me from my coursework to the
+          projects I've worked!
+        </text>
+        <div></div>
+      </section>
     </div>
   );
 };
